b57241b03db82f91e1930e1e62eb9e208da7208f
heriii1901-blip <heriii1901@gmail.com>
2026-09-22 22:19:08 +0700

    add file

---
+++ musikku-skeleton (1)/musikku/lib/player/player_controller.dart
@@ -0,0 +1,9 @@
+// Wrapper buat just_audio (atau player lain)
+
+class PlayerController {
+  // TODO: Future<void> play(Song song)
+  //   -> kalau song.localCachePath != null, play dari situ (offline)
+  //   -> kalau null, minta presigned URL dulu ke R2Service, baru streaming
+
+  // TODO: pause(), resume(), seek(), next(), previous()
+}
